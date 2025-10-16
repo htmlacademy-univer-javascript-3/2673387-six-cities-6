cbc5df4ecb9a155a747f4f3f93bac462d8babd50
EvGeniusM <thezhekychan@gmail.com>
2025-10-16 18:16:03 +0500

    added-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from '../src/components/app/app.tsx';
+
+const config = {
+  offerCount: 5,
+} as const;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +12,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App offerCount={config.offerCount} />
   </React.StrictMode>
 );
