5974cac2b1846027b2fd6f4d7f5193a3d41b31b8
EvGeniusM <thezhekychan@gmail.com>
2025-12-27 16:13:15 +0500

    wip

---
+++ src/components/offer-sort/offer-sort.tsx
@@ -0,0 +1,54 @@
+﻿import { useState } from 'react';
+import { SortOptions } from '../../const.ts';
+
+type OfferSortProps = {
+  activeOption: SortOptions;
+  onSorterChange: (sorter: SortOptions) => void;
+}
+
+function OfferSort({ activeOption, onSorterChange }: OfferSortProps): JSX.Element {
+  const [isOpened, setIsOpened] = useState(false);
+  const sortOptions = Object.values(SortOptions);
+
+  function handleTypeClick() {
+    setIsOpened((prevIsOpened) => !prevIsOpened);
+  }
+
+  function handleOptionClick(option: SortOptions) {
+    onSorterChange(option);
+    setIsOpened(false);
+  }
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by</span>
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={handleTypeClick}
+      >
+        {activeOption}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+
+      <ul
+        className={`places__options places__options--custom ${isOpened ? 'places__options--opened' : ''}`}
+      >
+        {sortOptions.map((option) => (
+          <li
+            key={option}
+            className={`places__option${option === activeOption ? ' places__option--active' : ''}`}
+            tabIndex={0}
+            onClick={() => handleOptionClick(option)}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+}
+
+export default OfferSort;
--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,7 @@
 ﻿import { createAction } from '@reduxjs/toolkit';
 import type {CityType, Offer} from '../types/offer';
+import {SortOptions} from '../const.ts';
 
 export const changeCity = createAction<CityType>('offers/changeCity');
 export const setOffers = createAction<Offer[]>('offers/setOffers');
+export const changeSort = createAction<SortOptions>('main/changeSort');
